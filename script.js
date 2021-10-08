dedf369b806949dcdeee37613c76d49a033d1563
islom-uzpy <92166518+islom-uzpy@users.noreply.github.com>
2021-10-08 18:58:26 +0500

    yukladim

---
+++ script.js
@@ -0,0 +1,8 @@
+function login() {
+    var ism=document.getElementById('ism').value;
+    var familya=document.getElementById('familya').value;
+    var email=document.getElementById('email').value; 
+    var parol=document.getElementById('parol').value;
+    document.write(ism+familya+email+parol)
+
+}
